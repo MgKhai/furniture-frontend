bc6fad0f82fca9f402a7ed86781c33f724b7729f
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-01-07 16:19:29 +0630

    feat:icons_add

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,7 +1,12 @@
+import MainNavigation from "@/components/layouts/MainNavigation.tsx";
+// import { siteConfig } from "@/config/site.ts";
+
 function Header() {
     return (
     <header className="w-full border-b">
-        <div className="container m-auto flex items-center  h-16">Navigation</div>
+        <div className="container m-auto flex items-center  h-16">
+            <MainNavigation />
+        </div>
     </header>
     );
 }
