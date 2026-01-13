633dd6fe6ac2746a606f38343a615ccba3792de0
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-01-13 10:56:17 +0630

    feat:home_herosection_finish

--- src/components/layouts/MobileNavigation.tsx
+++ src/components/layouts/MobileNavigation.tsx
@@ -88,18 +88,6 @@ function MobileNavigation({items}: MobileNavigationProps) {
                           )}
                       </div>
                   </ScrollArea>
-                  {/*<SheetHeader>*/}
-                  {/*    /!*<SheetTitle>Edit profile</SheetTitle>*!/*/}
-                  {/*    /!*<SheetDescription>*!/*/}
-                  {/*    /!*    Make changes to your profile here. Click save when you&apos;re done.*!/*/}
-                  {/*    /!*</SheetDescription>*!/*/}
-                  {/*</SheetHeader>*/}
-                  {/*<SheetFooter>*/}
-                  {/*    /!*<Button type="submit">Save changes</Button>*!/*/}
-                  {/*    /!*<SheetClose asChild>*!/*/}
-                  {/*    /!*    <Button variant="outline">Close</Button>*!/*/}
-                  {/*    /!*</SheetClose>*!/*/}
-                  {/*</SheetFooter>*/}
               </SheetContent>
           </Sheet>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,24 +6,24 @@ function Home() {
     return (
         <>
             <div className="container mx-auto">
-                <div className="flex flex-col lg:flex-row lg: justify-between items-center">
+                <div className="flex flex-col lg:flex-row lg: justify-between items-start">
                     {/*Text Section*/}
-                    <div className="flex flex-col text-center lg:text-left">
-                        <h1>Modern Interior Design Studio</h1>
-                        <p>Furniture is an essential component of any living space, providing
+                    <div className="flex flex-col text-center lg:text-left mt-8 lg:mt-20 gap-4 lg:gap-6 lg:w-2/5">
+                        <h1 className="text-4xl lg:text-6xl font-extrabold text-[#3b5d50]">Modern Interior Design Studio</h1>
+                        <p className="text-[#3b5d50]">Furniture is an essential component of any living space, providing
                             functionality, comfort, and aesthetic appeal.
                         </p>
-                        <div>
-                            <Button>
+                        <div className="flex gap-2 items-center justify-center lg:justify-start">
+                            <Button asChild className="px-6 py-6 rounded-full bg-orange-300 text-base font-bold">
                                 <Link to="#">Shop Now</Link>
                             </Button>
-                            <Button>
+                            <Button asChild variant="outline" className="text-[#3b5d50] px-6 py-6 rounded-full text-base font-bold">
                                 <Link to="#">Explore</Link>
                             </Button>
                         </div>
                     </div>
                     {/*Image Section*/}
-                    <img src={Couch} alt="Couch" className="" />
+                    <img src={Couch} alt="Couch" className="w-full lg:w-3/5" />
                 </div>
             </div>
         </>
