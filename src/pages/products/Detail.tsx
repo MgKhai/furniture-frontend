e82d97220e343a51609367e0984ef9dd869bfbc5
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-01-23 15:42:16 +0630

    feat:product_detail_design_add

--- src/pages/products/Detail.tsx
+++ src/pages/products/Detail.tsx
@@ -1,7 +1,67 @@
+import * as React from "react"
+import {Link, useParams} from "react-router";
+import {products} from "@/data/products.ts";
+import {Button} from "@/components/ui/button.tsx";
+import {Icons} from "@/components/ui/icons.tsx";
+import { Separator } from "@/components/ui/separator"
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import ProductCard from "@/components/products/ProductCard.tsx";
+
 function ProductDetail(){
+    const {productId} = useParams();
+    const product = products.find(product => product.id === productId);
+
+    const plugin = React.useRef(
+        Autoplay({ delay: 2000, stopOnInteraction: true })
+    )
+
     return (
-        <div>
-            Detail
+        <div className="container mx-auto px-4 lg:px-0 my-6">
+            <Button variant="outline" asChild >
+                <Link to="/product"> <Icons.arrowLeft aria-hidden="true" /> All Products </Link>
+            </Button>
+            <section className="flex flex-col md:flex-row mt-6 mb-15">
+
+                {/*Carousel Product Image*/}
+                <div className="md:w-1/2">
+                    <Carousel
+                        plugins={[plugin.current]}
+                        className="w-full"
+                        onMouseEnter={plugin.current.stop}
+                        onMouseLeave={plugin.current.reset}
+                    >
+                        <CarouselContent>
+                            {product?.images.map((image, index) => (
+                                <CarouselItem key={index}>
+                                    <img src={image} alt={product.name} className="size-full rounded-md object-cover" />
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                    </Carousel>
+                </div>
+
+                <Separator className="mt-4 md:hidden" />
+
+                <div className="md:w-1/2"></div>
+            </section>
+            <section>
+                <h2 className="line-clamp-1 text-2xl font-bold my-6">More Products From Furniture Shop</h2>
+
+                {/*Other Product Types Scroll Area*/}
+                <ScrollArea className="w-full rounded-md pb-4 whitespace-nowrap">
+                    <div className="flex gap-4 w-max">
+                        <ProductCard products={products.slice(0,4)} className="min-w-77"  />
+                    </div>
+                    <ScrollBar orientation="horizontal" />
+                </ScrollArea>
+            </section>
         </div>
     );
 }
