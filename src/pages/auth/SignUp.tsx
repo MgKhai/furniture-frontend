a30ccd28c2b9854b72bf4d871764c5ba889d0ac1
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-06-06 12:36:25 +0630

    feat:signup_otp_ui_added

---
+++ src/pages/auth/SignUp.tsx
@@ -0,0 +1,10 @@
+import { SignupForm } from "@/components/auth/SignUpForm";
+export default function SignupPage() {
+  return (
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
