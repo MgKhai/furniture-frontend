875032cd35d189e1e929538d6b68f915ed054cad
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-06-07 18:24:04 +0630

    feat:confirmPasswordForm_updated

--- src/components/auth/ConfirmPasswordForm.tsx
+++ src/components/auth/ConfirmPasswordForm.tsx
@@ -1,78 +1,174 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { FieldDescription, FieldGroup } from "@/components/ui/field";
 import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  FieldSeparator,
-} from "@/components/ui/field";
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { AlertCircleIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
+import { Link, useActionData, useNavigation, useSubmit } from "react-router";
+import { Icons } from "../ui/icons";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 
-export function ConfirmPasswordPage({
+const formSchema = z.object({
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(8, "Password must be at most 8 characters long")
+    .regex(/^\d+$/, "Password must contain only digits"),
+  confirmPassword: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(8, "Password must be at most 8 characters long")
+    .regex(/^\d+$/, "Password must contain only digits"),
+});
+
+export function ConfirmPasswordForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const submit = useSubmit();
+  const navigate = useNavigation();
+  const actionData = useActionData();
+  const [showPassword, setShowPassword] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    submit(values, { method: "post", action: "/register/confirm-password" });
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
-        <FieldGroup>
-          <div className="flex flex-col items-center gap-2 text-center">
-            <a
-              href="#"
-              className="flex flex-col items-center gap-2 font-medium"
-            >
-              <div className="flex size-8 items-center justify-center rounded-md">
-                <GalleryVerticalEnd className="size-6" />
-              </div>
-              <span className="sr-only">Acme Inc.</span>
-            </a>
-            <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
-            <FieldDescription>
-              Already have an account? <a href="#">Sign in</a>
-            </FieldDescription>
-          </div>
-          <Field>
-            <FieldLabel htmlFor="email">Email</FieldLabel>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </Field>
-          <Field>
-            <Button type="submit">Create Account</Button>
-          </Field>
-          <FieldSeparator>Or</FieldSeparator>
-          <Field className="grid gap-4 sm:grid-cols-2">
-            <Button variant="outline" type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                  fill="currentColor"
-                />
-              </svg>
-              Continue with Apple
-            </Button>
-            <Button variant="outline" type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                  fill="currentColor"
-                />
-              </svg>
-              Continue with Google
-            </Button>
-          </Field>
-        </FieldGroup>
-      </form>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-2 text-center">
+          <Link to="#" className="flex flex-col items-center gap-2 font-medium">
+            <div className="flex size-8 items-center justify-center rounded-md">
+              <Icons.logo className="size-8" aria-hidden="true" />
+            </div>
+            <span className="sr-only">Furniture Shop</span>
+          </Link>
+          <h1 className="text-xl font-bold">Please confirm password</h1>
+          <FieldDescription>
+            Passwords must be 8 characters and contain only digits.
+          </FieldDescription>
+        </div>
+        {actionData?.error && (
+          <Alert variant="destructive" className="mb-2 max-w-md">
+            <AlertCircleIcon />
+            <AlertTitle>Sign up failed</AlertTitle>
+            <AlertDescription>{actionData.message}</AlertDescription>
+          </Alert>
+        )}
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="grid w-full"
+            autoComplete="off"
+          >
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-end">
+                      <FormLabel className="text-sm">Password</FormLabel>
+                    </div>
+
+                    <div className="relative">
+                      <FormControl>
+                        <Input
+                          placeholder="********"
+                          type={showPassword ? "text" : "password"}
+                          inputMode="numeric"
+                          className="pr-10"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
+                        onClick={() => setShowPassword((prev) => !prev)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4" />
+                        ) : (
+                          <Eye className="h-4 w-4" />
+                        )}
+                      </Button>
+                    </div>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-end">
+                      <FormLabel className="text-sm">
+                        Confirm Password
+                      </FormLabel>
+                    </div>
+
+                    <div className="relative">
+                      <FormControl>
+                        <Input
+                          placeholder="********"
+                          type={showPassword ? "text" : "password"}
+                          inputMode="numeric"
+                          className="pr-10"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
+                        onClick={() => setShowPassword((prev) => !prev)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4" />
+                        ) : (
+                          <Eye className="h-4 w-4" />
+                        )}
+                      </Button>
+                    </div>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="mt-2">
+                {navigate.state === "submitting" ? "Signing up..." : "Confirm"}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </FieldGroup>
     </div>
   );
 }
