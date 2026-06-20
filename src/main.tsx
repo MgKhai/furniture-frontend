9511f5827255ac0c7f0e4fd184a77ee952238b7d
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-06-20 21:36:16 +0630

    feat:tanstack_query_config_added

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import { router } from "@/routes.tsx";
 import { RouterProvider } from "react-router/dom";
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./api/query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
